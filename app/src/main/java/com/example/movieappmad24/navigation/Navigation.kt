55fe05cc56b95625961715ad6191e3d0e673f1ed
Jeeems10 <jamesponce10@outlook.com>
2024-03-31 20:41:51 +0200

    Navigation Refactoring finished

--- app/src/main/java/com/example/movieappmad24/navigation/Navigation.kt
+++ app/src/main/java/com/example/movieappmad24/navigation/Navigation.kt
@@ -14,14 +14,14 @@ fun Navigation() {
     val navController = rememberNavController() // create a NavController instance
 
     NavHost(navController = navController, // pass the NavController to NavHost
-        startDestination = "homescreen") {  // pass a start destination
+        startDestination = Screen.HomeScreen.route) {  // pass a start destination
 
-        composable(route = "homescreen"){   // route with name "homescreen" navigates to HomeScreen composable
+        composable(route = Screen.HomeScreen.route){   // route with name "homescreen" navigates to HomeScreen composable
             HomeScreen(navController = navController)
         }
 
         composable(
-            route = "detailscreen/{movieId}",
+            route = Screen.DetailScreen.route,
             arguments = listOf(navArgument(name = "movieId") {type = NavType.StringType})
         ) { backStackEntry ->
             DetailScreen(navController = navController,movieId = backStackEntry.arguments?.getString("movieId"))
