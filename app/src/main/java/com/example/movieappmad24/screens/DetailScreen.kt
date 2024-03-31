0ca62838ed21649bddebc872e64c7e96d2527a84
Jeeems10 <jamesponce10@outlook.com>
2024-03-31 19:33:51 +0200

    DetailScreen a) finished

--- app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
+++ app/src/main/java/com/example/movieappmad24/screens/DetailScreen.kt
@@ -1,9 +1,41 @@
 package com.example.movieappmad24.screens
 
+import android.annotation.SuppressLint
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import com.example.movieappmad24.models.Movie
+import com.example.movieappmad24.models.getMovies
 
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun DetailScreen(movieId: String?) {
-    Text(text = "Hello detailscreen $movieId")
+fun DetailScreen(navController: NavController,movieId: String?) {
+    val movie = getMovieById(movieId)
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = movie?.title ?: "Movie Details") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Filled.ArrowBack, "Back")
+                    }
+                }
+            )
+        }
+    ) {
+        Text(text = "Hello detailscreen $movieId")
+    }
+}
+
+fun getMovieById(movieId: String?): Movie? {
+    return getMovies().find { it.id == movieId }
 }
